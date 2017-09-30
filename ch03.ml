15416b765a89963b561b8fd001a13943c78a9641
Hongchang Wu <wuhc85@gmail.com>
2017-09-30 00:06:37 -0400

    Reformat

--- ch03.ml
+++ ch03.ml
@@ -19,11 +19,10 @@ sig
   val deleteMin : heap -> heap   (* raises Empty if heap is empty *)
 end
 
-
-(** Page 18 to 19 - an implementation of leftist heap *)
-module LeftistHeap (O : Ordered) : Heap with module Elem = O =
+(** Page 32 - Leftist heaps. *)
+module LeftistHeap (Element : Ordered) : Heap with module Elem = Element =
 struct
-  module Elem = O
+  module Elem = Element
 
   type heap = E | T of int * Elem.t * heap * heap
 
@@ -36,17 +35,24 @@ struct
     | E -> 0
     | T (r, _, _, _) -> r
   let makeT (x, a, b) =
-    if rank a >= rank b then T (rank b + 1, x, a, b)
+    if rank a >= rank b
+    then T (rank b + 1, x, a, b)
     else T (rank a + 1, x, b, a)
 
   let rec merge = function
     | (h, E) -> h
     | (E, h) -> h
     | ((T (_, x, a1, b1) as h1), (T (_, y, a2, b2) as h2)) ->
-      if Elem.leq (x, y) then makeT (x, a1, merge (b1, h2))
+      if Elem.leq (x, y)
+      then makeT (x, a1, merge (b1, h2))
       else makeT (y, a2, merge (h1, b2))
 
   let insert (x, h) = merge (T (1, x, E, E), h)
-  let findMin (T (_, x, a, b)) = x
-  let deleteMin (T (_, x, a, b)) = merge (a, b)
+
+  let findMin = function
+    | E -> failwith "Empty"
+    | (T (_, x, a, b)) -> x
+  let deleteMin = function
+    | E -> failwith "Empty"
+    | (T (_, x, a, b)) -> merge (a, b)
 end
