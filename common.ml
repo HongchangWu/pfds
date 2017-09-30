6c3e6688645dd43322ee2eb379647cdfaa70da16
Hongchang Wu <wuhc85@gmail.com>
2017-09-30 01:00:13 -0400

    Re-indent common.ml

--- common.ml
+++ common.ml
@@ -1,10 +1,10 @@
 (** Page 14 - Implementation of binary search trees as a functor. *)
 module type Ordered =
-  (* a totally ordered type and its comparison functions *)
-  sig
-    type t
+(* a totally ordered type and its comparison functions *)
+sig
+  type t
 
-    val eq  : t * t -> bool
-    val lt  : t * t -> bool
-    val leq : t * t -> bool
-  end
+  val eq  : t * t -> bool
+  val lt  : t * t -> bool
+  val leq : t * t -> bool
+end
