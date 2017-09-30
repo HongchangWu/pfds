2a3e701e9e07ef18af6bcf088c625193410d42a9
Hongchang Wu <wuhc85@gmail.com>
2017-09-29 22:18:13 -0400

    Add leftist heap example

--- ch03.ml
+++ ch03.ml
@@ -18,3 +18,35 @@ sig
   val findMin   : heap -> Elem.t (* raises Empty if heap is empty *)
   val deleteMin : heap -> heap   (* raises Empty if heap is empty *)
 end
+
+
+(** Page 18 to 19 - an implementation of leftist heap *)
+module LeftistHeap (O : Ordered) : Heap with module Elem = O =
+struct
+  module Elem = O
+
+  type heap = E | T of int * Elem.t * heap * heap
+
+  let empty = E
+  let isEmpty = function
+    | E -> true
+    | _ -> false
+
+  let rank = function
+    | E -> 0
+    | T (r, _, _, _) -> r
+  let makeT (x, a, b) =
+    if rank a >= rank b then T (rank b + 1, x, a, b)
+    else T (rank a + 1, x, b, a)
+
+  let rec merge = function
+    | (h, E) -> h
+    | (E, h) -> h
+    | ((T (_, x, a1, b1) as h1), (T (_, y, a2, b2) as h2)) ->
+      if Elem.leq (x, y) then makeT (x, a1, merge (b1, h2))
+      else makeT (y, a2, merge (h1, b2))
+
+  let insert (x, h) = merge (T (1, x, E, E), h)
+  let findMin (T (_, x, a, b)) = x
+  let deleteMin (T (_, x, a, b)) = merge (a, b)
+end
