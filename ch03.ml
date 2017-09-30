78994b1d2aad183917d7d817f3d1c072a0584e11
Hongchang Wu <wuhc85@gmail.com>
2017-09-30 00:58:44 -0400

    Exercise 3.4

--- ch03.ml
+++ ch03.ml
@@ -93,3 +93,70 @@ struct
       |> List.map (fun x -> T (1, x, E, E))
       |> go
 end
+
+(** Page 19 - Exercise 3.4
+    Weight-biased leftist heaps are an alternative to leftist heaps that replace
+    the leftist property with the weight-biased leftist property: the size of
+    any child is at least as large as the size of its right siblings.
+*)
+module WeightLeftistHeap (Element : Ordered) : Heap with module Elem = Element =
+struct
+  module Elem = Element
+
+  type heap = E | T of int * Elem.t * heap * heap
+
+  let empty = E
+  let isEmpty = function
+    | E -> true
+    | _ -> false
+
+  let size = function
+    | E -> 0
+    | T (n, _, _, _) -> n
+  let makeT (x, a, b) =
+    if size a >= size b
+    then T (size a + size b, x, a, b)
+    else T (size a + size b, x, b, a)
+
+  let rec merge = function
+    | (h, E) -> h
+    | (E, h) -> h
+    | ((T (n1, x, a1, b1) as h1), (T (n2, y, a2, b2) as h2)) ->
+      if Elem.leq (x, y)
+      then
+        if size a1 + size b1 >= n2
+        then T (n1 + n2, x, merge (a1, a2), h2)
+        else T (n1 + n2, x, h2, merge (a1, a2))
+      else
+        begin
+          if size a2 + size b2 >= n1
+          then T (n1 + n2, y, merge (a2, b2), h1)
+          else T (n1 + n2, y, h1, merge (a2, b2))
+        end
+
+  let rec insert (x, t) =
+    match t with
+    | E ->
+      T (1, x, E, E)
+    | T (n, y, a, b) ->
+      if Elem.leq (x, y)
+      then
+        if size a = size b
+        then T (n + 1, x, insert (y, a), b)
+        else T (n + 1, x, a, insert (y, b))
+      else
+        begin
+          if size a = size b
+          then T (n + 1, y, insert (x, a), b)
+          else T (n + 1, y, a, insert (x, b))
+        end
+
+  let insert (x, h) = merge (T (1, x, E, E), h)
+
+  let findMin = function
+    | E -> failwith "Empty"
+    | (T (_, x, a, b)) -> x
+  let deleteMin = function
+    | E -> failwith "Empty"
+    | (T (_, x, a, b)) -> merge (a, b)
+end
