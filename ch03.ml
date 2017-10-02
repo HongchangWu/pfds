e33351006f14f9d050eda500420345cb62a7298f
Hongchang Wu <wuhc85@gmail.com>
2017-10-01 22:00:15 -0400

    Binomial heap

--- ch03.ml
+++ ch03.ml
@@ -160,3 +160,49 @@ struct
     | E -> failwith "Empty"
     | (T (_, x, a, b)) -> merge (a, b)
 end
+
+(** Page 24 - Binomial heaps.
+*)
+module BinomialHeap (Element : Ordered) : Heap with module Elem = Element =
+struct
+  module Elem = Element
+
+  type tree = Node of int * Element.t * tree list
+  type heap = tree list
+
+  let empty = []
+  let isEmpty = function [] -> true | _ -> false
+
+  let rank (Node (r, _, _)) = r
+  let root (Node (_, x, _)) = x
+  let link ((Node (r, x1, c1) as t1), (Node (_, x2, c2) as t2)) =
+    if Elem.leq (x1, x2)
+    then Node (r + 1, x1, t2 :: c1)
+    else Node (r + 1, x2, t1 :: c2)
+  let rec insTree = function
+    | (t, []) -> [t]
+    | (t, (t' :: ts' as ts)) ->
+      if rank t < rank t' then t :: ts else insTree (link (t, t'), ts')
+
+  let insert (x, ts) = insTree (Node (0, x, []), ts)
+  let rec merge = function
+    | (ts1, []) -> ts1
+    | ([], ts2) -> ts2
+    | ((t1 :: ts1' as ts1), (t2 :: ts2' as ts2)) ->
+      if rank t1 < rank t2 then t1 :: merge (ts1', ts2)
+      else if rank t2 < rank t1 then t2 :: merge (ts1, ts2')
+      else insTree (link (t1, t2), merge (ts1', ts2'))
+
+  let rec removeMinTree = function
+    | [] -> failwith "Empty"
+    | t :: ts ->
+      let t', ts' = removeMinTree ts in
+      if Elem.leq (root t, root t') then (t, ts) else (t', t :: ts')
+
+  let findMin ts =
+    let (t, _) = removeMinTree ts in
+    root t
+  let deleteMin ts =
+    let Node (_, x, ts1), ts2 = removeMinTree ts in
+    merge (List.rev ts1, ts2)
+end
