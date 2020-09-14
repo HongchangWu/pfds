953697a41dcbff852a81f97eb25cce32ecaa90ff
Hongchang Wu <wuhc85@gmail.com>
2020-09-13 22:38:22 -0400

    Format using ocamlformat

--- ch04.ml
+++ ch04.ml
@@ -2,54 +2,65 @@
 
 open Sigs
 
-module Stream : STREAM =
-struct
-  type 'a streamCell = Nil | Cons of 'a * 'a stream
+module Stream : STREAM = struct
+  type 'a streamCell =
+    | Nil
+    | Cons of 'a * 'a stream
+
   and 'a stream = 'a streamCell Lazy.t
 
-  let rec (++) s t =
+  let rec ( ++ ) s t =
     match Lazy.force s with
     | Nil -> t
     | Cons (x, s) -> lazy (Cons (x, s ++ t))
+  ;;
+
   let rec take n s =
-    if n = 0 then lazy Nil
-    else match Lazy.force s with
+    if n = 0
+    then lazy Nil
+    else (
+      match Lazy.force s with
       | Nil -> lazy Nil
-      | Cons (x, s) -> lazy (Cons (x, take (n - 1) s))
+      | Cons (x, s) -> lazy (Cons (x, take (n - 1) s)))
+  ;;
+
   let drop n s =
     let rec drop' n s =
       match Lazy.force s with
       | Nil -> Nil
       | Cons (_, s) -> drop' (n - 1) s
     in
-    if n = 0 then s
-    else lazy (drop' n s) (* delay the evaluation of drop', which is monolithic *)
+    if n = 0 then s else lazy (drop' n s)
+  ;;
+
+  (* delay the evaluation of drop', which is monolithic *)
+
   let reverse s =
     let rec reverse' s r =
       match Lazy.force s with
       | Nil -> r
       | Cons (x, s) -> reverse' s (Cons (x, lazy r))
     in
-    lazy (reverse' s Nil) (* delay the evaluation of reverse', which is monolithic *)
+    lazy (reverse' s Nil)
+  ;;
+
+  (* delay the evaluation of reverse', which is monolithic *)
 
   (** Page 37 - Exercise 4.2
       Implement insertion sort on streams. Show that extracting the first k elements of
       sort xs takes only O(n*k) time, where n is the length of xs, rather than O(n^2),
       as might be expected of insertion sort.
   *)
-  let insertionSort
-        (type a)
-        (module Ord : ORDERED with type t = a)
-        (xs : a stream) : a stream =
+  let insertionSort (type a) (module Ord : ORDERED with type t = a) (xs : a stream)
+      : a stream
+    =
     let rec insert x ys =
       match Lazy.force ys with
-      | Nil ->
-        lazy (Cons (x, lazy Nil))
+      | Nil -> lazy (Cons (x, lazy Nil))
       | Cons (y, ys) ->
-        if Ord.lt x y then
-          lazy (Cons (x, lazy (Cons (y, ys))))
-        else
-          lazy (Cons (y, insert x ys))
+        if Ord.lt x y
+        then lazy (Cons (x, lazy (Cons (y, ys))))
+        else lazy (Cons (y, insert x ys))
     in
     let rec insertionSort' xs =
       match Lazy.force xs with
@@ -57,4 +68,5 @@ struct
       | Cons (x, xs) -> insert x @@ insertionSort' xs
     in
     insertionSort' xs
+  ;;
 end
--- sigs.ml
+++ sigs.ml
@@ -2,83 +2,78 @@ exception Empty
 
 (** Page 8 - Figure 2.1.
     Signature for stacks. *)
-module type STACK =
-sig
+module type STACK = sig
   type 'a stack
 
-  val empty   : 'a stack
+  val empty : 'a stack
   val isEmpty : 'a stack -> bool
-
-  val cons    : 'a -> 'a stack -> 'a stack
-  val head    : 'a stack -> 'a
-  val tail    : 'a stack -> 'a stack
+  val cons : 'a -> 'a stack -> 'a stack
+  val head : 'a stack -> 'a
+  val tail : 'a stack -> 'a stack
 end
 
 (** Page 12 - Figure 2.7.
     Signature for sets. *)
-module type SET =
-sig
+module type SET = sig
   type elem
   type set
 
-  val empty  : set
+  val empty : set
   val insert : elem -> set -> set
   val member : elem -> set -> bool
 end
 
 (** Page 14 - Figure 2.9.
     Implementation of binary search trees as a functor. *)
-module type ORDERED =
-(* a totally ordered type and its comparison functions *)
+module type ORDERED = (* a totally ordered type and its comparison functions *)
 sig
   type t
 
-  val eq  : t -> t -> bool
-  val lt  : t -> t -> bool
+  val eq : t -> t -> bool
+  val lt : t -> t -> bool
   val leq : t -> t -> bool
 end
 
 (** Page 18 - Figure 3.1.
     Signature for heaps (priority queues). *)
-module type HEAP =
-sig
+module type HEAP = sig
   module Elem : ORDERED
 
   type heap
 
-  val empty     : heap
-  val isEmpty   : heap -> bool
+  val empty : heap
+  val isEmpty : heap -> bool
+  val insert : Elem.t -> heap -> heap
+  val merge : heap -> heap -> heap
+  val findMin : heap -> Elem.t (* raises Empty if heap is empty *)
 
-  val insert    : Elem.t -> heap -> heap
-  val merge     : heap -> heap -> heap
-
-  val findMin   : heap -> Elem.t (* raises Empty if heap is empty *)
-  val deleteMin : heap -> heap   (* raises Empty if heap is empty *)
+  val deleteMin : heap -> heap (* raises Empty if heap is empty *)
 end
 
 (** Page 36 - Figure 4.1.
     A small streams package. *)
-module type STREAM =
-sig
-  type 'a streamCell = Nil | Cons of 'a * 'a stream
+module type STREAM = sig
+  type 'a streamCell =
+    | Nil
+    | Cons of 'a * 'a stream
+
   and 'a stream = 'a streamCell Lazy.t
 
-  val (++)    : 'a stream -> 'a stream -> 'a stream
-  val take    : int -> 'a stream -> 'a stream
-  val drop    : int -> 'a stream -> 'a stream
+  val ( ++ ) : 'a stream -> 'a stream -> 'a stream
+  val take : int -> 'a stream -> 'a stream
+  val drop : int -> 'a stream -> 'a stream
   val reverse : 'a stream -> 'a stream
 end
 
 (** Page 42 - Figure 5.1.
     Signature for queues. *)
-module type QUEUE =
-sig
+module type QUEUE = sig
   type 'a queue
 
-  val empty   : 'a queue
+  val empty : 'a queue
   val isEmpty : 'a queue -> bool
+  val snoc : 'a queue -> 'a -> 'a queue
+  val head : 'a queue -> 'a (* raises Empty if heap is empty *)
 
-  val snoc    : 'a queue -> 'a -> 'a queue
-  val head    : 'a queue -> 'a       (* raises Empty if heap is empty *)
-  val tail    : 'a queue -> 'a queue (* raises Empty if heap is empty *)
+  val tail : 'a queue -> 'a queue (* raises Empty if heap is empty *)
 end
