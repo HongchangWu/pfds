4a00c552b472ca9cbc9d87dcde8e83722238a23a
Hongchang Wu <wuhc85@gmail.com>
2020-09-14 20:33:22 -0400

    Exercise 5.1

--- ch05.ml
+++ ch05.ml
@@ -19,3 +19,40 @@ module BatchedQueue : QUEUE = struct
 
   let tail (f, r) = match f with [] -> raise Empty | _ :: f -> checkf (f, r)
 end
+
+(** Page 44 - Exercise 5.1
+    This design can easily be extended to support the double-ended queue, or
+    deque, abstraction, which allows reads and writes to both ends of the queue
+    (see Figure 5.3). The invariant is updated to be symmetric in its treatment
+    of f and r: both are required to be non-empty whenever the deque contains
+    two or more elements. When one list becomes empty, we split the other list
+    in half and reverse one of the halves.
+
+    (a) Implement this version of the deques.
+    (b) Prove that each operatino takes O(1) amortized time using the potential
+        function phi(f, r) = abs(|f| - |r|), where abs is the absolute value
+        function. *)
+module BatchedDeque : DEQUE = struct
+  type 'a queue = 'a list * 'a list
+
+  let empty = ([], [])
+
+  let isEmpty (f, _) = match f with [] -> true | _ -> false
+
+  let checkf (f, r) = match (f, r) with
+    | [], r -> (List.rev r, [])
+    | f, [] -> ([], List.rev f)
+    | q -> q
+
+  let cons x (f, r)   = checkf (x :: f, r)
+
+  let head (f, _) = match f with [] -> raise Empty | x :: _ -> x
+
+  let tail (f, r) = match f with [] -> raise Empty | _ :: f -> checkf (f, r)
+
+  let snoc (f, r) x = checkf (f, x :: r)
+
+  let last (_, r) = match r with [] -> raise Empty | x :: _ -> x
+
+  let init (f, r) = match r with [] -> raise Empty | _ :: r -> checkf (f, r)
+end
--- sigs.ml
+++ sigs.ml
@@ -6,9 +6,13 @@ module type STACK = sig
   type 'a stack
 
   val empty : 'a stack
+
   val isEmpty : 'a stack -> bool
+
   val cons : 'a -> 'a stack -> 'a stack
+
   val head : 'a stack -> 'a
+
   val tail : 'a stack -> 'a stack
 end
 
@@ -16,10 +20,13 @@ end
     Signature for sets. *)
 module type SET = sig
   type elem
+
   type set
 
   val empty : set
+
   val insert : elem -> set -> set
+
   val member : elem -> set -> bool
 end
 
@@ -30,7 +37,9 @@ sig
   type t
 
   val eq : t -> t -> bool
+
   val lt : t -> t -> bool
+
   val leq : t -> t -> bool
 end
 
@@ -42,9 +51,13 @@ module type HEAP = sig
   type heap
 
   val empty : heap
+
   val isEmpty : heap -> bool
+
   val insert : Elem.t -> heap -> heap
+
   val merge : heap -> heap -> heap
+
   val findMin : heap -> Elem.t (* raises Empty if heap is empty *)
 
   val deleteMin : heap -> heap (* raises Empty if heap is empty *)
@@ -53,15 +66,16 @@ end
 (** Page 36 - Figure 4.1.
     A small streams package. *)
 module type STREAM = sig
-  type 'a streamCell =
-    | Nil
-    | Cons of 'a * 'a stream
+  type 'a streamCell = Nil | Cons of 'a * 'a stream
 
   and 'a stream = 'a streamCell Lazy.t
 
   val ( ++ ) : 'a stream -> 'a stream -> 'a stream
+
   val take : int -> 'a stream -> 'a stream
+
   val drop : int -> 'a stream -> 'a stream
+
   val reverse : 'a stream -> 'a stream
 end
 
@@ -71,9 +85,38 @@ module type QUEUE = sig
   type 'a queue
 
   val empty : 'a queue
+
   val isEmpty : 'a queue -> bool
+
   val snoc : 'a queue -> 'a -> 'a queue
+
   val head : 'a queue -> 'a (* raises Empty if heap is empty *)
 
   val tail : 'a queue -> 'a queue (* raises Empty if heap is empty *)
+end
+
+(** Page 45 - Figure 5.3.
+    Signature for double-ended queuese. *)
+module type DEQUE = sig
+  type 'a queue
+
+  val empty : 'a queue
+
+  val isEmpty : 'a queue -> bool
+
+  (* insert, inspect, and remove the front element *)
+
+  val cons : 'a -> 'a queue -> 'a queue
+
+  val head : 'a queue -> 'a (* raises Empty if heap is empty *)
+
+  val tail : 'a queue -> 'a queue (* raises Empty if heap is empty *)
+
+  (* insert, inspect, and remove the rear element *)
+
+  val snoc : 'a queue -> 'a -> 'a queue
+
+  val last : 'a queue -> 'a (* raises Empty if heap is empty *)
+
+  val init : 'a queue -> 'a queue (* raises Empty if heap is empty *)
 end
