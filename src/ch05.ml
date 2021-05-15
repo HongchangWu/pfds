f5b68a12975d050b02db954ee443f6c082f73aa9
Hongchang Wu <wuhc85@gmail.com>
2021-05-15 13:26:47 -0400

    Figure 5.5. splay trees

--- src/ch05.ml
+++ src/ch05.ml
@@ -73,3 +73,61 @@ module BatchedDeque : DEQUE = struct
 
   let init (f, r) = match r with [] -> raise Empty | _ :: r -> checkf (f, r)
 end
+
+(** Page 50 - Figure 5.5.
+    Implementaiton of heaps using splay trees. *)
+module SplayHeap (Element : ORDERED) : HEAP with module Elem = Element = struct
+  module Elem = Element
+
+  type heap = E | T of heap * Elem.t * heap
+
+  let empty = E
+
+  let isEmpty = function E -> true | _ -> false
+
+  let rec partition pivot = function
+    | E -> (E, E)
+    | T (a, x, b) as t -> (
+        if Elem.leq x pivot then
+          match b with
+          | E -> (t, E)
+          | T (a', y, b') ->
+              if Elem.leq y pivot then
+                let small, big = partition pivot b' in
+                (T (T (a, x, a'), y, small), big)
+              else
+                let small, big = partition pivot a' in
+                (T (a, x, small), T (big, y, b'))
+        else
+          match a with
+          | E -> (E, t)
+          | T (a', y, b') ->
+              if Elem.leq y pivot then
+                let small, big = partition pivot b' in
+                (T (a', y, small), T (big, x, b))
+              else
+                let small, big = partition pivot a' in
+                (small, T (big, y, T (b', x, b))))
+
+  let insert x t =
+    let a, b = partition x t in
+    T (a, x, b)
+
+  let rec merge t1 t2 =
+    match (t1, t2) with
+    | E, t -> t
+    | T (a, x, b), t ->
+        let ta, tb = partition x t in
+        T (merge ta a, x, merge tb b)
+
+  let rec findMin = function
+    | E -> raise Empty
+    | T (E, x, _) -> x
+    | T (a, _, _) -> findMin a
+
+  let rec deleteMin = function
+    | E -> raise Empty
+    | T (E, _, b) -> b
+    | T (T (E, _, b), y, c) -> T (b, y, c)
+    | T (T (a, x, b), y, c) -> T (deleteMin a, x, T (b, y, c))
+end
